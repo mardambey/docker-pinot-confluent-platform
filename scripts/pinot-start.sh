a354fa855ea19e020135f07d61c9a7c4228023fc
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2016-02-06 16:02:35 -0500

    Delete sources when done, respect ZK_ADDRESS env variabe.

--- scripts/pinot-start.sh
+++ scripts/pinot-start.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
+cd /opt/pinot-0.016-pkg/
+
 #bin/pinot-admin.sh StartZookeeper &
 #sleep 2
-bin/pinot-admin.sh StartController &
+
+if [ -n "$ZK_ADDRESS" ]; then
+	ZK_OPT="-zkAddress $ZK_ADDRESS"
+fi
+
+bin/pinot-admin.sh StartController $ZK_OPT &
 sleep 2
-bin/pinot-admin.sh StartBroker &
+
+bin/pinot-admin.sh StartBroker $ZK_OPT &
 sleep 2
-bin/pinot-admin.sh StartServer &
-#sleep 2
+
+bin/pinot-admin.sh StartServer $ZK_OPT &
+sleep 2
+
 #bin/pinot-admin.sh StartKafka &
+#sleep 2
 
 wait
