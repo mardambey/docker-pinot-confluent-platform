255f8a364c0f0d1e66fc9e05046787fd3fdb591a
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2016-02-13 19:09:47 -0500

    Move data and logs into specific directories.

--- scripts/pinot-start.sh
+++ scripts/pinot-start.sh
@@ -2,6 +2,11 @@
 
 cd /opt/pinot-0.016-pkg/
 
+mkdir -p /pinot-controller/data \
+  /pinot-broker \
+  /pinot-server/segments \
+  /pinot-server/data &> /dev/null
+
 #bin/pinot-admin.sh StartZookeeper &
 #sleep 2
 
@@ -9,13 +14,13 @@ if [ -n "$ZK_ADDRESS" ]; then
 	ZK_OPT="-zkAddress $ZK_ADDRESS"
 fi
 
-bin/pinot-admin.sh StartController $ZK_OPT &
+bin/pinot-admin.sh StartController $ZK_OPT -dataDir /pinot-controller/data | tee /pinot-controller/controller.log &
 sleep 2
 
-bin/pinot-admin.sh StartBroker $ZK_OPT &
+bin/pinot-admin.sh StartBroker $ZK_OPT | tee /pinot-broker/broker.log &
 sleep 2
 
-bin/pinot-admin.sh StartServer $ZK_OPT &
+bin/pinot-admin.sh StartServer $ZK_OPT -segmentDir /pinot-server/segments -dataDir /pinot-server/data | tee /pinot-server/server.log &
 sleep 2
 
 #bin/pinot-admin.sh StartKafka &
