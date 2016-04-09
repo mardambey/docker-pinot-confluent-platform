d126c56bcf3b54195e5b7cdf871b27805ca511f2
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2016-04-09 13:10:18 -0400

    Remove docker from image's name.

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 echo Building latest...
 git co master
-docker build -t mardambey/docker-pinot-confluent-platform:latest .
+docker build -t mardambey/pinot-confluent-platform:latest .
 
 echo Building 1.0.1
 git co 0.16-confluent-1.0.1
-docker build -t mardambey/docker-pinot-confluent-platform:0.16-confluent-1.0.1 .
+docker build -t mardambey/pinot-confluent-platform:0.16-confluent-1.0.1 .
 
 echo Building 2.0.0
 git co 0.16-confluent-2.0.0
-docker build -t mardambey/docker-pinot-confluent-platform:0.16-confluent-2.0.0 .
+docker build -t mardambey/pinot-confluent-platform:0.16-confluent-2.0.0 .
 
 git co master
